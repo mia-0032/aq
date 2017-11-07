ef0648beff0b59b3243835d635a4fdbc402bb1ab
Yoshihiro MIYAI <msparrow17@gmail.com>
2017-11-07 15:42:28 +0900

    Add `load` command

--- lib/aq/query_builder.rb
+++ lib/aq/query_builder.rb
@@ -17,5 +17,24 @@ module Aq
         raise InvalidParameterError.new 'Use `load` command if you create new table.'
       end
     end
+
+    def self.load(table, source, schema, source_format, patition)
+      raise InvalidParameterError.new '`SOURCE` must start with "s3://"' unless source.start_with? 's3://'
+      schema_state = schema.get_all.map do |s|
+        "`#{s[:name]}` #{s[:type]}"
+      end.join(',')
+      raise InvalidParameterError.new 'Now aq support only NEWLINE_DELIMITED_JSON.' unless %w(NEWLINE_DELIMITED_JSON).include? source_format
+      serde = 'org.apache.hive.hcatalog.data.JsonSerDe'
+      patition_state = patition.nil? ? '' : "PARTITIONED BY (#{patition.gsub(':', ' ')})"
+
+      <<"SQL"
+CREATE EXTERNAL TABLE IF NOT EXISTS #{table} (
+#{schema_state}
+)
+#{patition_state}
+ROW FORMAT SERDE "#{serde}"
+LOCATION "#{source}"
+SQL
+    end
   end
 end
