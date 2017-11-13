9c5489a69c0bb56756a173a872f75cd034e6f45a
Yoshihiro MIYAI <msparrow17@gmail.com>
2017-11-13 15:05:01 +0900

    Make `object_prefix` become `class_option` and set default value

--- lib/aq/command.rb
+++ lib/aq/command.rb
@@ -11,44 +11,37 @@ require 'aq/version'
 module Aq
   class AqCmd < Thor
     package_name 'aq'
+    class_option :object_prefix, desc: 'S3 object prefix where the query result is stored', default: "Unsaved/#{Date.today.strftime("%Y/%m/%d")}"
 
     desc "ls [DATABASE]", "Show databases or tables in specified database"
     option :bucket, desc: 'S3 bucket where the query result is stored.', required: true
-    option :object_prefix, desc: 'S3 object prefix where the query result is stored. Default is `Unsaved/%Y/%m/%d`'
     def ls(database=nil)
       query = QueryBuilder.ls database
-      object_prefix = options[:object_prefix] || "Unsaved/#{Date.today.strftime("%Y/%m/%d")}"
-      Aq::Query.new(options[:bucket], object_prefix).run(query)
+      Aq::Query.new(options[:bucket], options[:object_prefix]).run(query)
     end
 
     desc "mk NAME", "Create database"
     option :bucket, desc: 'S3 bucket where the query result is stored.', required: true
-    option :object_prefix, desc: 'S3 object prefix where the query result is stored. Default is `Unsaved/%Y/%m/%d`'
     def mk(name)
       query = QueryBuilder.mk name
-      object_prefix = options[:object_prefix] || "Unsaved/#{Date.today.strftime("%Y/%m/%d")}"
-      Aq::Query.new(options[:bucket], object_prefix).run(query)
+      Aq::Query.new(options[:bucket], options[:object_prefix]).run(query)
     end
 
     desc "load TABLE SOURCE SCHEMA", "Create table and load data"
     option :bucket, desc: 'S3 bucket where the query result is stored.', required: true
-    option :object_prefix, desc: 'S3 object prefix where the query result is stored. Default is `Unsaved/%Y/%m/%d`'
     option :source_format, desc: 'Specify source file data format. Now aq support only NEWLINE_DELIMITED_JSON.', default: 'NEWLINE_DELIMITED_JSON'
     option :partitioning, desc: 'Specify partition key and type. ex. key1:type1,key2:type2,...', default: nil
     def load(table, source, schema)
       schema = SchemaLoader.new.load schema
       query = QueryBuilder.load table, source, schema, options[:source_format], options[:partitioning]
-      object_prefix = options[:object_prefix] || "Unsaved/#{Date.today.strftime("%Y/%m/%d")}"
-      Aq::Query.new(options[:bucket], object_prefix).run(query)
+      Aq::Query.new(options[:bucket], options[:object_prefix]).run(query)
     end
 
     desc "query QUERY", "Run the query"
     option :bucket, desc: 'S3 bucket where the query result is stored.', required: true
-    option :object_prefix, desc: 'S3 object prefix where the query result is stored. Default is `Unsaved/%Y/%m/%d`'
     option :timeout, desc: 'Wait for execution of the query for this number of seconds', default: nil, type: :numeric
     def query(query)
-      object_prefix = options[:object_prefix] || "Unsaved/#{Date.today.strftime("%Y/%m/%d")}"
-      Aq::Query.new(options[:bucket], object_prefix).run(query, options[:timeout])
+      Aq::Query.new(options[:bucket], options[:object_prefix]).run(query, options[:timeout])
     end
 
     desc "version", "Print the version"
